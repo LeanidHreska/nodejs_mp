074163969a86ca1792758918e89fc6484fc870a6
Leanid Hreska <Leanid_Hreska@epam.com>
2018-04-12 13:18:11 +0300

    setting up eslint, add npx to devDependency

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
-import Product from "./Product";
-import User from "./User";
+import Product from './Product';
+import User from './User';
 
 const models = {
   User,
   Product
-}
+};
 
 export default models;
